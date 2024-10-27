021c014888c5dc06c66202c3dfcbe987cf50a78f
Indrajit <indrajit.kumar@philips.com>
2024-10-27 12:51:09 +0530

    Implemented Generate report in Excel Sheets

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 import requests
+import pandas as pd
+import os
 
 # Create Flask app instance
 app = Flask(__name__)
@@ -8,137 +10,86 @@ app.secret_key = "supersecretkey"  # Used for flashing messages
 # Black Duck API URL (replace with your Black Duck instance URL)
 BLACKDUCK_URL = "https://blackduck.philips.com"
 
-# Define the route for the homepage (login form)
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-# Define the route to authenticate and get projects
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
-    # Get the API token from the form
     api_token = request.form.get('api_token')
-
-    # Black Duck API endpoint to authenticate and get the bearer token
     url = f"{BLACKDUCK_URL}/api/tokens/authenticate"
     payload = {}
-    # Set up headers for the API request
     headers = {
         "Authorization": f"token {api_token}",
         "Accept": "application/vnd.blackducksoftware.user-4+json"
     }
-
-    # Make the request to the Black Duck API to authenticate
-
     response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
-
-    # Check if the request was successful
     if response.status_code == 200:
         token_data = response.json()
-        session['bearerToken'] = token_data['bearerToken']  # Store the token in the session
-        return redirect(url_for('projects'))  # Redirect to the projects page
+        session['bearerToken'] = token_data['bearerToken']
+        return redirect(url_for('projects'))
     else:
-        # If authentication fails, flash an error message and redirect to the login page
         flash("Failed to authenticate with the provided token. Please try again.")
         return redirect(url_for('home'))
 
-    # Define the route to get projects
-
-
 @app.route('/projects')
 def projects():
-    # Get the bearer token from the session
     bearer_token = session.get('bearerToken')
-
     if not bearer_token:
         flash("You need to authenticate first.")
         return redirect(url_for('home'))
-
-    # Black Duck API endpoint to get projects
     url = f"{BLACKDUCK_URL}/api/projects"
-
-    # Set up headers for the API request
     headers = {
         "Authorization": f"Bearer {bearer_token}",
         "Accept": "application/json"
     }
-
-    # Make the request to the Black Duck API to fetch projects
     response = requests.get(url, headers=headers)
-
-    # Check if the request was successful
     if response.status_code == 200:
-        projects_data = response.json().get('items', [])  # Extract projects from the response
-        return render_template('projects.html', projects=projects_data)  # Pass data to the template
+        projects_data = response.json().get('items', [])
+        return render_template('projects.html', projects=projects_data)
     else:
-        # If fetching projects fails, flash an error message and redirect to the login page
         flash("Failed to fetch projects. Please try again.")
         return redirect(url_for('home'))
 
 @app.route('/projects/<project_id>')
 def get_project_versions(project_id):
-    # Get the bearer token from the session
     bearer_token = session.get('bearerToken')
     if not bearer_token:
         flash("You need to authenticate first.")
         return redirect(url_for('home'))
-
-    # Black Duck API endpoint to get project versions
     url = f"{BLACKDUCK_URL}/api/projects/{project_id}/versions"
-
-    # Set up headers for the API request
     headers = {
         "Authorization": f"Bearer {bearer_token}",
         "Accept": "application/json"
     }
-
-    # Make the request to the Black Duck API to fetch project versions
     response = requests.get(url, headers=headers)
-
-    # Check if the request was successful
     if response.status_code == 200:
         project_versions = response.json().get('items', [])
         component_overview = []
         seen_components = set()
         for version in project_versions:
-            print(version.get('versionName', 'No name found'))
             version_href = version['_meta']['href']
-            print(version_href)
-            get_version_components(version_href, component_overview, seen_components)
-            print(component_overview)
+            component_overview = get_version_components(version_href, component_overview, seen_components)
         return {'status': 'success', 'data': component_overview}
     else:
         return {'status': 'error', 'message': 'Failed to fetch project versions'}
 
-
 def get_version_components(versionIds, component_overview, seen_components):
-    filters = '?filter=bomInclusion:false&filter=bomMatchInclusion:false&filter=bomMatchReviewStatus:reviewed&filter=securityRisk:high&filter=securityRisk:medium&limit=100&offset=0'
-    # Get the bearer token from the session
+    filters = 'filter=bomInclusion:false&filter=bomMatchInclusion:false&filter=bomMatchReviewStatus:reviewed&filter=securityRisk:high&filter=securityRisk:medium&limit=100&offset=0'
     bearer_token = session.get('bearerToken')
     if not bearer_token:
         flash("You need to authenticate first.")
         return redirect(url_for('home'))
-
-    # Black Duck API endpoint to get project versions
-    url = f"{versionIds}/components{filters}"
-
-    # Set up headers for the API request
+    url = f"{versionIds}/components?{filters}"
     headers = {
         "Authorization": f"Bearer {bearer_token}",
         "Accept": "application/json"
     }
-
-    # Make the request to the Black Duck API to fetch project versions
     response = requests.get(url, headers=headers)
-    # Check if the request was successful
     if response.status_code == 200:
         filtered_components = response.json().get('items', [])
         for component in filtered_components:
             component_name = component.get('componentName', 'No name found')
-
             if component_name not in seen_components:
                 seen_components.add(component_name)
                 component_overview.append({
@@ -147,7 +98,6 @@ def get_version_components(versionIds, component_overview, seen_components):
                     'componentDescription': get_component_details(component.get('component')),
                     'componentVersion': get_component_version_details(component.get('componentVersion'))
                 })
-
         return component_overview
     else:
         return {'status': 'error', 'message': 'Failed to fetch project versions'}
@@ -157,22 +107,14 @@ def get_component_details(param):
     if not bearer_token:
         flash("You need to authenticate first.")
         return redirect(url_for('home'))
-
-    # Black Duck API endpoint to get project versions
     url = f"{param}"
-
-    # Set up headers for the API request
     headers = {
         "Authorization": f"Bearer {bearer_token}",
         "Accept": "application/json"
     }
-
-    # Make the request to the Black Duck API to fetch project versions
     response = requests.get(url, headers=headers)
-    # Check if the request was successful
     if response.status_code == 200:
         description = response.json().get('description', [])
-        print(description)
         return description
     else:
         return {'status': 'error', 'message': 'Failed to fetch project versions'}
@@ -183,19 +125,13 @@ def get_component_version_details(param):
     if not bearer_token:
         flash("You need to authenticate first.")
         return redirect(url_for('home'))
-
-    # Black Duck API endpoint to get project versions
     url = f"{param}{vulnerabilities}"
-
-    # Set up headers for the API request
     headers = {
         "Authorization": f"Bearer {bearer_token}",
         "Accept": "application/json"
     }
     vulnerabilities = []
-    # Make the request to the Black Duck API to fetch project versions
     response = requests.get(url, headers=headers)
-    # Check if the request was successful
     if response.status_code == 200:
         component_version_details = response.json()
         if component_version_details.get('totalCount', 0) > 0:
@@ -213,8 +149,81 @@ def get_component_version_details(param):
         return vulnerabilities
     else:
         return {'status': 'error', 'message': 'Failed to fetch project versions'}
+@app.route('/generate_excel_report/<project_id>')
+def generate_excel_report(project_id):
+    bearer_token = session.get('bearerToken')
+    if not bearer_token:
+        flash("You need to authenticate first.")
+        return redirect(url_for('home'))
+    url = f"{BLACKDUCK_URL}/api/projects/{project_id}/versions"
+    headers = {
+        "Authorization": f"Bearer {bearer_token}",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        project_versions = response.json().get('items', [])
+        component_overview = []
+        seen_components = set()
+        for version in project_versions:
+            version_href = version['_meta']['href']
+            component_overview = get_version_components(version_href, component_overview, seen_components)
 
+        # Ensure the download directory exists
+        download_dir = 'download'
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir)
 
-# Run the app
+        file_path = os.path.join(download_dir, 'component_overview.xlsx')
+        save_to_excel(component_overview, file_path)
+        return send_file(file_path, as_attachment=True)
+    else:
+        return {'status': 'error', 'message': 'Failed to fetch project versions'}
+
+def save_to_excel(component_overview, file_path='component_overview.xlsx'):
+    with pd.ExcelWriter(file_path) as writer:
+        # Title Page
+        title_page_df = pd.DataFrame([{'Title': 'Black Duck Projects Report'}])
+        title_page_df.to_excel(writer, sheet_name='Title Page', index=False)
+
+        # Component Overview
+        component_overview_df = pd.DataFrame(
+            [{k: v for k, v in item.items() if k != 'componentVersion'} for item in component_overview])
+        component_overview_df.to_excel(writer, sheet_name='Component Overview', index=False)
+
+        # Analysis
+        analysis_data = []
+        for item in component_overview:
+            for version in item['componentVersion']:
+                analysis_data.append({
+                    'Review date (YYYY-MM-DD)': version['vulnerabilityUpdatedDate'],
+                    'Component name': item['componentName'],
+                    'Component version': item['componentVersionName'],
+                    'Vulnerability ID (e.g. CVE)': version['vulnerabilityName'],
+                    'Description': version['description'],
+                    'Base score': version['baseScore'],
+                    'Exploitability': version['exploitabilitySubscore'],
+                    'Impact': version['impactSubscore'],
+                    'Remediation status': version.get('remediationStatus', ''),
+                    'Remediation comment': version.get('remediationComment', ''),
+                    'Severity rating': version['severity'],
+                    'Update PSSD?': 'No',
+                    'Manage Defect requested?': 'No'
+                })
+        analysis_df = pd.DataFrame(analysis_data)
+        analysis_df.to_excel(writer, sheet_name='Analysis', index=False)
+
+        # Document Revision History
+        revision_history_df = pd.DataFrame([{'Revision': '1.0', 'Date': '2023-10-01', 'Description': 'Initial version'}])
+        revision_history_df.to_excel(writer, sheet_name='Document Revision History', index=False)
+
+        # Terminology & Abbreviations
+        abbreviations_df = pd.DataFrame([{'Term': 'PSSD', 'Definition': 'Product Security Software Development'}])
+        abbreviations_df.to_excel(writer, sheet_name='Terminology & Abbreviations', index=False)
+
+        # References
+        references_df = pd.DataFrame([{'Reference': 'Black Duck API Documentation', 'Link': 'https://blackducksoftware.github.io/'}])
+        references_df.to_excel(writer, sheet_name='References', index=False)
+
 if __name__ == '__main__':
     app.run(debug=True)
